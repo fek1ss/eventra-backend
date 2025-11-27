6773393f03ebd15fb2763581342261e3099300c1
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-28 00:33:12 +0600

    Registration to event

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -3,23 +3,30 @@ const db = require('../config/db');
 // Регистрация на мероприятие
 exports.registerForEvent = async (req, res) => {
   try {
-    const { event_id, user_id, first_name, last_name, phone, isPaid } = req.body;
+    const { event_id, user_id, first_name, last_name, phone } = req.body;
 
+    // Получаем название мероприятия
+    const [rows] = await db.query("SELECT title FROM events WHERE id = ?", [event_id]);
+    const eventTitle = rows[0]?.title || "Мероприятие";
+
+    // Записываем регистрацию
     await db.query(
       'INSERT INTO registrations (event_id, user_id, first_name, last_name, phone) VALUES (?, ?, ?, ?, ?)',
       [event_id, user_id, first_name, last_name, phone]
     );
 
-    if (isPaid) {
-      const whatsappLink = `https://wa.me/1234567890?text=Я хочу купить билет на мероприятие ${event_id}`;
-      return res.json({ message: 'Регистрация успешна', whatsappLink });
-    }
 
-    res.json({ message: 'Регистрация успешна' });
-  } catch (err) {
+    const phoneNumber = "+77056689441";
+
+    const encodedTitle = encodeURIComponent(eventTitle);
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=Здравствуйте, пишу вам с платформы Eventra. Мероприятие: ${encodedTitle}`;
+
+    return res.json({ message: "Регистрация успешна", whatsappLink });
+      } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
 
 // История мероприятий пользователя
 exports.getUserEvents = async (req, res) => {
